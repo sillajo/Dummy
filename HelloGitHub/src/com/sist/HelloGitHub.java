b7dfb59f0dd3d1c93073e7efafcc65b9e51d13f2
silla <23rn@naver.com>
2017-02-06 15:07:20 +0900

    HelloGitHub modified

---
+++ HelloGitHub/src/com/sist/HelloGitHub.java
@@ -0,0 +1,16 @@
+package com.sist;
+
+public class HelloGitHub {
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+		System.out.println("What's Git?");
+        System.out.println("It's DVCS");
+        System.out.println("It's collaboration");
+        System.out.println("Test");
+        System.out.println("Test");
+        System.out.println("Test");
+        
+        
+
+}
+}
